a0e646d18a4eeaa328e23f3429de3db49f4dba2d
kyle <kylechen@amazon.com>
2023-08-29 06:29:57 -0700

    temp

--- tool/get_job_id.dart
+++ tool/get_job_id.dart
@@ -1,24 +1,41 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:args/args.dart';
 
 void main(List<String> args) {
   final parser = ArgParser()
     ..addOption('substring')
-    ..addOption('job-json');
+    ..addOption('jobs-json-file');
 
   final results = parser.parse(args);
 
   final substring = results['substring']?.trim();
-  final jobJson = results['job-json']?.trim();
+  final jobsJsonFile = results['jobs-json-file'];
 
-  // Find the job whose name contains the substring
-  // Then return that job's id as a string
+  if (substring == null || substring.isEmpty) {
+    throw Exception('arguement: substring is required');
+  }
 
-  final job =
-      jobJson.split('\n').firstWhere((element) => element.contains(substring));
-  final jobId = job.split('"id": "')[1].split('"')[0];
+  if (jobsJsonFile == null || jobsJsonFile.isEmpty) {
+    throw Exception('arguement: jobs-json-file is required');
+  }
 
-  print(jobId);
+  final jobsJson = json.decode(File(jobsJsonFile).readAsStringSync())
+      as Map<String, dynamic>;
+
+  final jobList = jobsJson['jobs'] as List<dynamic>;
+
+  try {
+    final jobJson =
+        jobList.firstWhere((element) => element['name'].contains(substring));
+    final jobId = jobJson['databaseId'] as int;
+    print(jobId);
+  } on Exception catch (_) {
+    // Return empty string if no job found
+    print("");
+  }
 }
