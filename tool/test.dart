8211b732a80ea303b9e092abb730f0b1998a1068
kyle <kylechen@amazon.com>
2023-09-05 09:02:30 -0700

    dedicated test 1
    trigger
    
    testing suite
    
    try new output mechansim
    
    clean
    
    proper piping
    
    remove extraneous “
    
    additional behavior tests
    
    NEXT STEPS

---
+++ tool/test.dart
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/// Parse and send metric data using AWS CLI
+void main(List<String> args) {
+  print('Hello from Dart!');
+}
